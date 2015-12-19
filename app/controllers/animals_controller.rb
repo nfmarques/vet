4f0f34574c6f932ed80c12d0ae9b82c98add81c7
nelsonjma <nelsonjma@hotmail.com>
2015-12-19 19:43:17 +0000

    building clinic views/controllers

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,34 +1,24 @@
 class AnimalsController < ApplicationController
+
+  before_action :get_clinic, only: [:new, :create, :show, :edit, :update, :destroy]
   before_action :set_animal, only: [:show, :edit, :update, :destroy]
 
-  # GET /animals
-  # GET /animals.json
-  def index
-    @animals = Animal.all
-  end
-
-  # GET /animals/1
-  # GET /animals/1.json
   def show
   end
 
-  # GET /animals/new
   def new
-    @animal = Animal.new
+    @animal = @clinic.animals.new
   end
 
-  # GET /animals/1/edit
   def edit
   end
 
-  # POST /animals
-  # POST /animals.json
   def create
-    @animal = Animal.new(animal_params)
+    @animal = @clinic.animals.new(animal_params)
 
     respond_to do |format|
       if @animal.save
-        format.html { redirect_to @animal, notice: 'Animal was successfully created.' }
+        format.html { redirect_to clinic_animal_path(@clinic, @animal), notice: 'Animal was successfully created.' }
         format.json { render :show, status: :created, location: @animal }
       else
         format.html { render :new }
@@ -37,12 +27,10 @@ class AnimalsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /animals/1
-  # PATCH/PUT /animals/1.json
   def update
     respond_to do |format|
       if @animal.update(animal_params)
-        format.html { redirect_to @animal, notice: 'Animal was successfully updated.' }
+        format.html { redirect_to clinic_animal_path(@clinic, @animal), notice: 'Animal was successfully updated.' }
         format.json { render :show, status: :ok, location: @animal }
       else
         format.html { render :edit }
@@ -51,24 +39,25 @@ class AnimalsController < ApplicationController
     end
   end
 
-  # DELETE /animals/1
-  # DELETE /animals/1.json
   def destroy
     @animal.destroy
     respond_to do |format|
-      format.html { redirect_to animals_url, notice: 'Animal was successfully destroyed.' }
+      format.html { redirect_to clinic_path(@clinic), notice: 'Animal was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_animal
-    @animal = Animal.find(params[:id])
+
+  def get_clinic
+    @clinic = Clinic.find(params[:clinic_id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  def set_animal
+    @animal = @clinic.animals.find(params[:id])
+  end
+
   def animal_params
-    params[:animal]
+    params.require(:animal).permit(:animal_name, :owner_name, :birth_date, :weigth)
   end
 end
